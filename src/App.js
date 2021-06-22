23a2e0671bb1161cd44e5b1bf829b440b559ee2e
Whadgu <wintahadgu@yahoo.com>
2021-06-22 02:19:27 -0700

    react app with gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className= "container">
+    <div className="learn">
+        <h1>Learn to code by watching others</h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            See how exprience developers solve problems in real-time.
+            Watching scripted tutorial is great, but understanding how developers think is invaluable.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    </div> 
+    <div className=" form-container">
+        <div className="try">
+            <b>try try free 7 days</b><a href="/" className="link-1">then $20/mo. thereater</a>
+        </div>
+        <form action="">
+            <input type="text" name="first_name" placeholder="First Name"/>
+            <input type="text" name="last_name" placeholder="Last Name"/>
+            <input type="email" name="email" placeholder="Email Address"/>
+            <input type="password" name="password" placeholder="Password"/>
+            <button type="submit" className="form-btn">CLAIM YOUR FREE TRIAL</button>
+        </form>
     </div>
+</div>
   );
 }
 
